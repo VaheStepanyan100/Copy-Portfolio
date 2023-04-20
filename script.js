fe74fadd8b8fec999c06a5b963f08a3c25ba76fb
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-04-20 16:56:27 +0400

    Copy Portfolio

---
+++ script.js
@@ -0,0 +1,65 @@
+const hamburgerIcon = document.querySelector('.hamburgerIcon');
+const menulist = document.querySelector('.menu-list-items > ul');
+const menuListItems = document.querySelectorAll('.menu-list-items li');
+
+hamburgerIcon.addeventListener('click', () => {
+  menulist.classList.toggle('active');
+});
+
+menuListItems.forEach((el) => {
+  el.addEventListener('click', () => {
+    menulist.classList.toggle('activ');
+  });
+});
+
+// Validate Form
+
+const form = document.querySelector('#form');
+const errorElement = document.querySelector('.error-element');
+const emailInput = form.elements.email;
+const submit = document.querySelector('#get-in-touch-button');
+
+function showError(event) {
+  errorElement.textContent = '*The email address is not provided.';
+  if (emailInput.value && emailInput.value !== emailInput.value.toLowerCase()) {
+    errorElement.textContent = '*The email address should be in lowercase.';
+    event.preventDefault();
+  }
+}
+
+submit.addEventListener('click', showError);
+
+// Local Storage
+
+const fullNameInput = form.name;
+const messageInput = form.message;
+
+const formObj = {
+  fullName: '',
+  email: '',
+  message: '',
+};
+
+if (localStorage.formSorage) {
+  formObj.fullName = JSON.parse(localStorage.formStorage).fullName;
+  formObj.email = JSON.parse(localStorage.formStorage).email;
+  formObj.message = JSON.parse(localStorage.formStorage).message;
+  fullNameInput.value = formObj.fullName;
+  emailInput.value = formObj.email;
+  messageInput.value = formObj.message;
+}
+
+fullNameInput.addEventListener('input', () => {
+  formObj.fullName = fullNameInput.value;
+  localStorage.setItem('formStorage', JSON.stringify(formObj));
+});
+
+emailInput.addEventListener('input', () => {
+  formObj.email = emailInput.value;
+  localStorage.setItem('formStorage', JSON.stringify(formObj));
+});
+
+messageInput.addEventListener('input', () => {
+  formObj.message = messageInput.value;
+  localStorage.setItem('formStorage', JSON.stringify(formObj));
+});
